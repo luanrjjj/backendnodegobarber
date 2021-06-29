ff199a6a03cad2184a23a9245c31b829643e2147
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-06-28 22:45:42 -0300

    Recuperacao e Reset de Senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,8 @@
 import IUsersRepository from '../repositories/IUsersRepository'
 import IMailProvider from '../../../shared/providers/MailProvider/models/IMailProvider'
 import { inject, injectable } from 'tsyringe';
+import AppError from '../../../shared/errors/AppError';
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
 interface IRequest {
     email:string;
@@ -14,10 +16,25 @@ class SendForgotPasswordEmailService{
 
         @inject('MailProvider')
         private mailProvider: IMailProvider,
+
+        @inject('UserTokensRepository')
+        private userTokensRepository: IUserTokensRepository,
     ){}
 
 
     public async execute({email}:IRequest): Promise<void> {
+
+        const user = await this.usersRepository.findByEmail(email);
+
+
+        if (!user) {
+            throw new AppError('User does not exists.')
+        }
+
+        
+
+        await this.userTokensRepository.generate(user.id)
+
         this.mailProvider.sendMail( email,'Pedido de Recuperação de Senha recebido.',)
 
        
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -7,7 +7,8 @@ import IUsersRepository from '../../modules/users/repositories/IUsersRepository'
 import UsersRepository from '../../modules/users/infra/typeorm/repositories/UsersRepository'
 import '../providers'
 
-
+// import IUserTokensRepository from '../../modules/users/repositories/IUsersRepository'
+// import UserTokensRepository from '../../modules/users/infra/typeorm/repositories/UsersRepository'
 
 
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository',delay(()=>AppointmentsRepository))
