0557ec08ec2d2e57370f85a44bf04f157362af93
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-07-03 16:20:51 -0300

    Rota e Controller de Perfil

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -7,6 +7,9 @@ import usersRouter from '../../../../modules/users/infra/http/routes/users.route
 import sessionsRouter from '../../../../modules/users/infra/http/routes/sessions.routes';
 
 import passwordRouter from '../../../../modules/users/infra/http/routes/password.routes'
+
+import profileRouter from '../../../../modules/users/infra/http/routes/profile.routes'
+
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
@@ -17,6 +20,7 @@ routes.use('/sessions', sessionsRouter);
 
 routes.use('/password',passwordRouter)
 
+routes.use('/profile',profileRouter);
 
 export default routes;
 
