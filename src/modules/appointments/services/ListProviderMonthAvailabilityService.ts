0e49c7b96e9099287d72466a7ac400dc08228b24
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-07-04 11:48:09 -0300

    Listando dias disponiveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-
+import {getDaysInMonth,getDate} from 'date-fns'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -33,9 +33,27 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year,month-1));
+
+    const eachDayArray = Array.from({
+      length:numberOfDaysInMonth
+    },(value,index)=>index+1,)
+
+    const availability = eachDayArray.map(day=>{
+      const appointmentsInDay = appointments.filter (appointment=>{
+        return getDate(appointment.date)===day;
+
+      })
+      return {
+        day,
+        available:appointmentsInDay.length<10
+      }
+    })
+
     console.log(appointments);
 
-    return [{ day: 1, available: false }];
+    return availability
   }
 }
 
