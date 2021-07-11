4d76885001bff51b3b969de4344da95a9432278f
Luan Alfredo Freitas Dias <luanfreitas12@poli.ufrj.br>
2021-07-11 14:35:08 -0300

    Organizando o Container

--- src/shared/providers/index.ts
+++ src/shared/providers/index.ts
@@ -1,35 +1,3 @@
-import IStorageProvider from './StorageProvider/models/IStorageProvider'
-
-import { container } from 'tsyringe';
-
-import mailConfig from '../../config/mail'
-
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
-
-import IMailProvider from './MailProvider/models/IMailProvider'
-import EtherealMailProvider from '../providers/MailProvider/implementations/EtherealMailProvider'
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider'
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider'
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
-
-container.registerSingleton<IStorageProvider> (
-    'StorageProvider',
-    DiskStorageProvider
-);
-
-container.registerSingleton<IMailTemplateProvider> (
-    'MailTemplateProvider',
-    HandlebarsMailTemplateProvider
-);
-
-container.registerInstance<IMailProvider> (
-    'MailProvider',
-    mailConfig.driver ==='ethereal'
-    ?container.resolve(EtherealMailProvider)
-    :container.resolve(SESMailProvider)
-);
-
-
-
-
+import './StorageProvider';
+import './MailTemplateProvider';
+import './MailProvider';
